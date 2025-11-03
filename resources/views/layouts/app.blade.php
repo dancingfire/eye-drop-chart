bbdb0fca01e109483e6fca0d75adcd6c24e03d5f
Gord Funk <gord.funk@gmail.com>
2025-11-03 16:27:24 -0600

    Logo updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,10 @@
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="{{ route('dashboard') }}">{{ config('app.name', 'Eye Drop Chart') }}</a>
+            <a class="navbar-brand d-flex align-items-center" href="{{ route('dashboard') }}">
+                <img src="{{ asset('images/logo.png') }}" alt="Logo" height="30" class="me-2">
+                {{ config('app.name', 'Eye Drop Chart') }}
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
